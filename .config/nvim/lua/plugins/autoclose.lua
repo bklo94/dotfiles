4a0b33e03e18f31d48d02ef1b89f682b8d7f5743
Brandon Lo <11641062+bklo94@users.noreply.github.com>
2025-04-19 17:51:24 -0700

    Add autoclose plugin

---
+++ .config/nvim/lua/plugins/autoclose.lua
@@ -0,0 +1,11 @@
+return {
+	"m4xshen/autoclose.nvim",
+	version = false,
+	config = function()
+		require("autoclose").setup({
+			keys = {
+				["$"] = { escape = true, close = true, pair = "$$", disabled_filetypes = {} },
+			},
+		})
+	end,
+}
