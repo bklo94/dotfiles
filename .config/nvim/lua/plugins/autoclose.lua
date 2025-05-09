f6b762d66b57ab104fb097f68c07ce2467fdc00a
Brandon Lo <11641062+bklo94@users.noreply.github.com>
2025-05-09 04:00:10 -0700

    Update configs

--- .config/nvim/lua/plugins/autoclose.lua
+++ .config/nvim/lua/plugins/autoclose.lua
@@ -4,7 +4,7 @@ return {
 	config = function()
 		require("autoclose").setup({
 			keys = {
-				["$"] = { escape = true, close = true, pair = "$$", disabled_filetypes = {} },
+				["$"] = { escape = true, close = true, pair = "$", disabled_filetypes = {} },
 			},
 		})
 	end,
